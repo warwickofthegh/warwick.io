3d122905cc961d86a763028c4cd3081f17f876bc
warwickofthegh <dubsix@gmail.com>
2020-08-17 12:46:32 +0930

    updates

--- dist/app.js
+++ dist/app.js
@@ -1,54 +1,56 @@
 // get items from database
-async function getItems(){
-    const response = await fetch('data.json');
-    const data = await response.json();
-    return data;
+async function getItems() {
+  const response = await fetch('data.json');
+  const data = await response.json();
+  return data;
 }
 
 // call get items function and handle return promise
 getItems()
-    .then(data => createItems(data.items))
-    .catch(err => console.log(err));
+  .then((data) => createItems(data.items))
+  .catch((err) => console.log(err));
 
 // create Items and inject into DOM
-function createItems(items){
-
-    itemHTML = '';
-    items.forEach((item) => {
-
-        //build links html
-        let itemLinksHTML = ''
-        const links = item.links;
-        // add link lis
-        if(links){
-            links.forEach((link)=>{
-                itemLinksHTML += `
+function createItems(items) {
+  itemHTML = '';
+  items.forEach((item) => {
+    //build links html
+    let itemLinksHTML = '';
+    const links = item.links;
+    // add link lis
+    if (links) {
+      links.forEach((link) => {
+        itemLinksHTML += `
                     <li><a href="${link.url}" target="_blank" alt="${link.title}">${link.title}</a></li>
                 `;
-            });
-        } 
-        // add ul
-        if(links){
-            itemLinksHTML = `<ul>${itemLinksHTML}</ul>`;
-        }
+      });
+    }
+    // add ul
+    if (links) {
+      itemLinksHTML = `<ul>${itemLinksHTML}</ul>`;
+    }
 
-        // add item
-        if(item.type === 'project'){
-            itemHTML += `
+    // add item
+    if (item.type === 'project') {
+      itemHTML += `
                 <div class="item">
-                    <img src="${item.imageUrl}" alt="${item.title}">
-                    <p class="body">${item.body}</p>
-                    ${itemLinksHTML}
+                    <div class"item-cont">
+                        <img src="${item.imageUrl}" alt="${item.title}">
+                        <p class="body">${item.body}</p>
+                        ${itemLinksHTML}
+                    </div>
                 </div>
             `;
-        } else if(item.type === 'image'){
-            itemHTML += `
+    } else if (item.type === 'image') {
+      itemHTML += `
                 <div class="item">
-                    <img src="${item.imageUrl}" alt="${item.title}">
+                    <div class"item-cont">
+                        <img src="${item.imageUrl}" alt="${item.title}">
+                    </div>
                 </div>
             `;
-        }
-    })
-    //inject items into dom
-    document.getElementById('items').innerHTML = itemHTML;
+    }
+  });
+  //inject items into dom
+  document.getElementById('items').innerHTML = itemHTML;
 }
